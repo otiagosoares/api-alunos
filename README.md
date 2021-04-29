3b37aaad249c0c64cb417fa45ae606ba3baf0697
Tiago Soares <fullcodebr@gmail.com>
2021-04-29 09:23:08 -0300

    .env config + read.me

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # api-alunos
+
+##installl:
+npx i
+
+##migrations
+npx sequelize migration:create --name=entitie_name
+npx sequelize db:migrate
+
+##init:
+npm run dev
+
+##.env
+DATABASE_NAME=db_name
+DATABASE_HOST=localhost
+DATABASE_PORT=3307
+DATABASE_USERNAME=root
+DATABASE_PASSWORD=
+
+
--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   dialect: 'mariadb',
   host: process.env.DATABASE_HOST,
-  database: process.env.DATABASE_DATABASE,
+  database: process.env.DATABASE_NAME,
   port: process.env.DATABASE_PORT,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
