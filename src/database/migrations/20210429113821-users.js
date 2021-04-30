791e21c04f322efd1bcef8cb972569bdd3f9c5eb
Tiago Soares <fullcodebr@gmail.com>
2021-04-30 17:58:53 -0300

    fix migrations users

--- src/database/migrations/20210429113821-users.js
+++ src/database/migrations/20210429113821-users.js
@@ -21,11 +21,11 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      createdAt: {
+      created_at: {
         type: Sequelize.DATE,
         allowNull: false,
       },
-      updatedAt: {
+      updated_at: {
         type: Sequelize.DATE,
         allowNull: false,
       },
